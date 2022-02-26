ef1eefafbb2b7bd10e94d814815f990346beb74e
Georg Wicke-Arndt <georg.wicke@rwth-aachen.de>
2022-02-26 13:28:26 +0100

    Initial commit

---
+++ logger/MyLogger.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Client;
+
+[FriendlyName("MyLogger")]
+[ExtensionUri("some://uri/notsurewhatssupposedtogohere")]
+public class MyLogger : ITestLogger
+{
+    public void Initialize(TestLoggerEvents events, string testRunDirectory)
+    {
+        StreamWriter writer = new StreamWriter("./output.txt");
+        writer.AutoFlush = true;
+
+        events.DiscoveredTests += (sender, e) =>
+        {
+            foreach (var testCase in e.DiscoveredTestCases)
+            {
+                writer.WriteLine($"FullyQualifiedName: {testCase.FullyQualifiedName}");
+                writer.WriteLine($"Location: {testCase.CodeFilePath ?? "<null>"}:{testCase.LineNumber}");
+                writer.WriteLine();
+            }
+        };
+    }
+}
